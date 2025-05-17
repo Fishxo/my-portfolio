97c6c9654ace0a86e8a91574660ee6bd67bbb2a0
Fishxo <137769496+Fishxo@users.noreply.github.com>
2025-05-17 08:23:48 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import Hero from './components/hero/Hero'
+import Hero from './components/hero/Hero';
 import Aos from 'aos'
 import 'aos/dist/aos.css'
 import Skills from './components/skills/Skills';
