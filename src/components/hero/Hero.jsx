a12553c96c91d0795e4ed27cc9447bb623414812
Fishxo <137769496+Fishxo@users.noreply.github.com>
2025-05-17 08:38:31 +0300

    Update Hero.jsx

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../navbar/Navbar'
+import Navbar from '../navbar/Navbar.jsx'
 import hi from '../../assets/hi.png.webp';
 import cv from './cv.pdf'
 export default function Hero() {
